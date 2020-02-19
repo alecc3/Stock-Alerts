ed4d629a78332294624292ab4c2871a71cc273ac
Alec Chen <altqqftw@gmail.com>
2020-02-18 17:28:54 -0800

    edit Create Alert fields

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -36,8 +36,7 @@ export default class Create extends Component {
             done: false
         }
         this.onChangeName = this.onChangeName.bind(this);
-        this.onChangeBuy = this.onChangeBuy.bind(this);
-        this.onChangeSell = this.onChangeSell.bind(this);
+        this.onChangePrice = this.onChangePrice.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
     }
     autocomplete(){
@@ -67,14 +66,9 @@ export default class Create extends Component {
         });
         this.forceUpdate();
     }
-    onChangeBuy(e) {
+    onChangePrice(e) {
         this.setState({
-            buy: e.target.value
-        });
-    }
-    onChangeSell(e) {
-        this.setState({
-            sell: e.target.value
+            price: e.target.value
         });
     }
     onSubmit(e) {
@@ -109,8 +103,9 @@ export default class Create extends Component {
                 <h3>Create New Alert</h3>
                 <form onSubmit={this.onSubmit}>
                     <div className="form-group"> 
-                        <label>Name: </label>
+                        <label>Ticker: </label>
                         <input  type="text"
+                                style={{ width:"300px" }}
                                 className="form-control"
                                 value={this.state.name}
                                 onChange={this.onChangeName}
@@ -121,27 +116,17 @@ export default class Create extends Component {
                         <p key={index}>${stock.ticker} - {stock.name}</p>
                         ))} */}
                     </div>
-                    <div>
-                        <Sync></Sync>
-                    </div>
                     <div className="form-group"> 
-                        <label>Buy Price: </label>
+                        <label>Alert Price: </label>
                         <input  type="text"
+                                style={{ width:"300px" }}
                                 className="form-control"
-                                value={this.state.buy}
+                                value={this.state.price}
                                 onChange={this.onChangeBuy}
                                 />
                     </div>
-                    <div className="form-group"> 
-                        <label>Sell Price: </label>
-                        <input  type="text"
-                                className="form-control"
-                                value={this.state.sell}
-                                onChange={this.onChangeSell}
-                                />
-                    </div>
                 <div className="form-group">
-                    <input type="submit" value="yeet" className="btn btn-primary" />
+                    <input type="submit" value="Add" className="btn btn-primary" />
                 </div>
                 </form>
             </div>
