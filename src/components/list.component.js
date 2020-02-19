8f7f59a18a0c09c8c0c19d80cba76c1a196cf6e1
Alec Chen <altqqftw@gmail.com>
2020-02-18 16:58:47 -0800

    added clock

--- src/components/list.component.js
+++ src/components/list.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import Clock from 'react-live-clock';
 
 const API = process.env.REACT_APP_IEX;
 
@@ -11,12 +12,7 @@ const Stock = props => (
     </tr>
 )
 
-const Price = props => (
-    <tr>
-        <td>{props.prices}</td>
-    </tr>
-)
-
+var zone = Intl.DateTimeFormat().resolvedOptions().timeZone
 
 export default class List extends Component {
     constructor(props) {
@@ -27,27 +23,18 @@ export default class List extends Component {
         };
     }
     componentDidMount() {
-        axios.get(`https://cloud.iexapis.com/stable/tops?token=${API}&symbols=aapl`).then(response => {
-            var res = response.data[0].symbol + " - " + "Apple"
+        axios.get(`https://cloud.iexapis.com/stable/tops?token=${API}&symbols=aapl,fb,amzn,googl`).then(response => {
+            var res = "$" + response.data[0].symbol + " - " + "Apple"
             var price = "$" + response.data[0].lastSalePrice.toFixed(2)
             this.setState({stocks: [...this.state.stocks, res], prices: [...this.state.prices, price]})
-        })
-
-        axios.get(`https://cloud.iexapis.com/stable/tops?token=${API}&symbols=fb`).then(response => {
-            var res = response.data[0].symbol + " - " + "Facebook"
-            var price = "$" + response.data[0].lastSalePrice.toFixed(2)
+            var res = "$" + response.data[1].symbol + " - " + "Facebook"
+            var price = "$" + response.data[1].lastSalePrice.toFixed(2)
             this.setState({stocks: [...this.state.stocks, res], prices: [...this.state.prices, price]})
-        })
-
-        axios.get(`https://cloud.iexapis.com/stable/tops?token=${API}&symbols=amzn`).then(response => {
-            var res = response.data[0].symbol + " - " + "Amazon"
-            var price = "$" + response.data[0].lastSalePrice.toFixed(2)
+            var res = "$" + response.data[2].symbol + " - " + "Amazon"
+            var price = "$" + response.data[2].lastSalePrice.toFixed(2)
             this.setState({stocks: [...this.state.stocks, res], prices: [...this.state.prices, price]})
-        })
-
-        axios.get(`https://cloud.iexapis.com/stable/tops?token=${API}&symbols=msft`).then(response => {
-            var res = response.data[0].symbol + " - " + "Microsoft"
-            var price = "$" + response.data[0].lastSalePrice.toFixed(2)
+            var res = "$" + response.data[3].symbol + " - " + "Alphabet"
+            var price = "$" + response.data[3].lastSalePrice.toFixed(2)
             this.setState({stocks: [...this.state.stocks, res], prices: [...this.state.prices, price]})
         })
 
@@ -58,22 +45,32 @@ export default class List extends Component {
         //     .catch(function (error){
         //         console.log(error);
         //     })
-        console.log(this.state.stocks)
     }
     display() {
         return this.state.stocks.map((t,i)=>{
             return <Stock tickers={t} prices={this.state.prices[i]}/>;
         })
     }
+    marketStatus(){
+        var date = new Date();
+        let hours = date.getHours();
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        if (hours > 1 && ampm == "pm"){
+            return <p>Market is Closed</p>
+        }
+        return <p>Market is Open</p>
+    }
     render() {
         return (
             <div>
+                Time - <Clock format={'h:mm:ss'} ticking={true} timezone={zone}/> {new Date().toString().match(/\(([A-Za-z\s].*)\)/)[1]}
+                {this.marketStatus()}
                 <h3>Alerts</h3>
                 <table className="table table-striped" style={{ marginTop: 20 }} >
                     <thead>
                         <tr>
                             <th>Ticker</th>
-                            <th>Price</th>
+                            <th>Current Price</th>
                         </tr>
                     </thead>
                     <tbody>
